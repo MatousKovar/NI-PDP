176a29701a42fe8b0f31c2e22bad701589477c0d
Matous Kovar <matouskovar1@gmail.com>
2026-02-21 09:06:01 +0100

    todo assert failing when testing piece placing logic

--- main.cpp
+++ main.cpp
@@ -1,28 +1,20 @@
 #include <iostream>
 #include <exception>
 #include "common/Board.h"
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+#include "tests/TestPiecesLogic.cpp"
+
 int main() {
-    std::cout << "=== StartedAlgorithm ===" << std::endl;
+    std::cout << "=== NI-PDP: SQX (Skladani quatromin s maximem) ===\n";
 
     try {
-        Board board;
-
-        board.loadFromFile("../data/mapb6_6c.txt");
-
-        std::cout << "Mapa uspesne nactena!" << std::endl;
-        std::cout << "Rozmery: " << board.getWidth() << "x" << board.getHeight() << std::endl;
-        std::cout << "Trivialni horni mez: " << board.getTrivialUpperBound() << "\n" << std::endl;
-
-        std::cout << "Pocatecni stav desky:" << std::endl;
-        board.printSolution();
+        // Založíme instanci testovací třídy a spustíme testy
+        TestPiecesLogic tests;
+        tests.runAllTests();
 
     } catch (const std::exception& e) {
         std::cerr << "Kriticka chyba: " << e.what() << std::endl;
-        return 1; // Ukončíme s chybovým kódem
+        return 1;
     }
 
-
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
-}
+}    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
---
+++ tests/TestPiecesLogic.cpp
@@ -0,0 +1,49 @@
+//
+// Created by Matouš Kovář on 21.02.2026.
+//
+#include <iostream>
+#include <cassert>
+#include "../common/Board.h"
+#include "../common/Pieces.h"
+
+class TestPiecesLogic {
+public:
+    static void runAllTests() {
+        std::cout << "--- Spoustim sanity testy ---\n";
+        test_loadFromFile();
+        test_placement_and_removal();
+        std::cout << "--- Vsechny testy OK ---\n\n";
+    }
+
+private:
+    static void test_loadFromFile() {
+        Board board;
+        board.loadFromFile("../data/mapb6_6c.txt");
+
+        assert(board.getWidth() == 6);
+        assert(board.getHeight() == 6);
+        assert(board.getTrivialUpperBound() == 280);
+
+        std::cout << "  [OK] test_loadFromFile proslo.\n";
+    }
+
+    static void test_placement_and_removal() {
+        Board board;
+        board.loadFromFile("../data/mapb6_6c.txt");
+
+        assert(board.canPlacePiece(0, Pieces::VARIANTS[0]) == true);
+        board.placePiece(0, Pieces::VARIANTS[0], 1);
+        assert(board.getTheoreticalMaxPossibleCost() == 240);
+
+        assert(board.canPlacePiece(0, Pieces::VARIANTS[0]) == false);
+        assert(board.canPlacePiece(1, Pieces::VARIANTS[0]) == false);
+        assert(board.canPlacePiece(2, Pieces::VARIANTS[0]) == false);
+
+        board.removePiece(0, Pieces::VARIANTS[0]);
+
+        assert(board.canPlacePiece(2, Pieces::VARIANTS[0]) == true);
+        assert(board.getTheoreticalMaxPossibleCost() == 280);
+
+        std::cout << "  [OK] test_placement_and_removal proslo.\n";
+    }
+};
