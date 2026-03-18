304ca410fb941c5870ea6b7d067c7de68797a6fb
Matous Kovar <matouskovar1@gmail.com>
2026-03-18 09:49:39 +0100

    updating mpi solution. SolveDFS now has omp usage

--- solvers/MpiSolver.cpp
+++ solvers/MpiSolver.cpp
@@ -126,6 +126,7 @@ void MpiSolver::solveDFS(Board board, int start_idx, int piece_id, int depth = 2
 
     int cell = board.getNextFreeCell(start_idx);
 
+    // Konec desky - kontrola a vraceni v rekurzi
     if (cell == -1)
     {
         if (board.getCurrentCost() > best_cost)
@@ -144,20 +145,40 @@ void MpiSolver::solveDFS(Board board, int start_idx, int piece_id, int depth = 2
 
     int cell_val = board.getCellValue(cell);
 
+    //VETVENI PODLE HODNOTY POLICKA
     if (cell_val > 0)
     {
         board.markAsEmpty(cell);
-        solveDFS(board, cell + 1, piece_id);
+
+        if (depth < z) // prepinani mezi sekvencnim a vicevlaknovym vetvenim, z by mela byt mala konstanta - napr 2, at se vytvori dost tasku, ktere budou vypocetne narocne, ne naopak
+        {
+            #pragma omp task shared(best_cost, best_board) // predavame globalni promenne do tasku
+            solveDFS(board, cell + 1, piece_id, depth + 1);
+        }
+        else
+            solveDFSSeq(board, cell + 1, piece_id);
         board.unmarkAsEmpty(cell);
 
-        if (best_cost == board.getTrivialUpperBound()) return;
+        if (best_cost == board.getTrivialUpperBound())
+        {
+            #pragma omp taskwait // Kdyz koncime predcasne, musime pockat nez se ukonci vsechny tasky v tomto vlaknu - sdileny zasobnik
+            return;
+        }
 
-        for (int i = 0; i < 12; ++i)
+
+        for (int i = 0; i < 12; ++i) // pokladani dilku
         {
             if (board.canPlacePiece(cell, Pieces::VARIANTS[i]))
             {
                 board.placePiece(cell, Pieces::VARIANTS[i], piece_id);
-                solveDFS(board, cell + 1, piece_id);
+                if (depth < z)
+                {
+                    #pragma omp task shared(best_cost, best_board)
+                    solveDFS(board, cell + 1, piece_id, depth + 1); // Vytvoříme úkol
+                }
+                else
+                    solveDFSSeq(board, cell + 1, piece_id); // Jdeme do sekvence
+
                 board.removePiece(cell, Pieces::VARIANTS[i]);
             }
         }
@@ -169,17 +190,38 @@ void MpiSolver::solveDFS(Board board, int start_idx, int piece_id, int depth = 2
             if (board.canPlacePiece(cell, Pieces::VARIANTS[i]))
             {
                 board.placePiece(cell, Pieces::VARIANTS[i], piece_id);
-                solveDFS(board, cell + 1, piece_id);
+
+                if (depth < z)
+                {
+                    #pragma omp task shared(best_cost, best_board)
+                    solveDFS(board, cell + 1, piece_id, depth + 1);
+                }
+                else
+                    solveDFSSeq(board, cell + 1, piece_id);
+
                 board.removePiece(cell, Pieces::VARIANTS[i]);
             }
         }
 
-        if (best_cost == board.getTrivialUpperBound()) return;
+        if (best_cost == board.getTrivialUpperBound())
+        {
+            #pragma omp taskwait
+            return;
+        }
 
         board.markAsEmpty(cell);
-        solveDFS(board, cell + 1, piece_id);
+        if (depth < z)
+        {
+            #pragma omp task shared(best_cost, best_board)
+            solveDFS(board, cell + 1, piece_id, depth + 1);
+        }
+        else
+            solveDFSSeq(board, cell + 1, piece_id);
         board.unmarkAsEmpty(cell);
     }
+
+    // ceka se na dokonceni vsech mnou vytvorenych tasku
+    #pragma omp taskwait
 }
 
 void MpiSolver::solveDFSSeq(Board &board, int start_idx, int piece_id)
